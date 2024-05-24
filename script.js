d8766f625bc140203fa42900574242ce9a95fec7
Josh Koszarek <112987840+joshkoszarek@users.noreply.github.com.>
2024-05-23 22:56:24 -0500

    Add basic math functions
    Two numbers are handled in the basic math operations of adding, subtracting, multiplying, dividing, and getting the remainder.

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
-console.log('hello there'); 
+//console.log('hello there'); 
+
+function add(num1, num2){ 
+    return num1 + num2; 
+}
+
+function subtract(num1, num2){ 
+    return num1 - num2; 
+}
+
+function multiply(num1, num2){ 
+    return num1 * num2;
+}
+
+function divide(num1, num2){ 
+    return num1 / num2; 
+}
+
+function getRemainder(num1, num2){ 
+    return num1 % num2; 
+}
+
 
