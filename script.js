fbe6174d50b3180e326499598a88e70d285a29be
Josh Koszarek <112987840+joshkoszarek@users.noreply.github.com.>
2024-05-23 23:10:43 -0500

    Add operate function to do an operation on 2 numbers

--- script.js
+++ script.js
@@ -21,3 +21,25 @@ function getRemainder(num1, num2){
 }
 
 
+function operate(num1, num2, operator){ 
+    switch(operator){ 
+        case '+':
+            return add(num1,num2);
+        case '-':
+            return subtract(num1, num2);
+        case '/': 
+            return divide(num1, num2); 
+        case '*': 
+            return multiply(num1, num2); 
+        case '%': 
+            return getRemainder(num1, num2); 
+    };
+}; 
+
+console.log(operate(1, 3, '+')); 
+console.log(operate(1, 3, '-')); 
+console.log(operate(1, 3, '*'));
+console.log(operate(1, 3, '/')); 
+console.log(operate(1, 3, '%'));  
+
+
